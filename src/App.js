c22964f0c703912a5c37f1d602b64e97eaca2cf7
haeyong9701 <haeyong9701@gmail.com>
2024-03-12 02:23:58 +0900

    chore: 리액트 쿼리 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import HomePage from "./HomePage";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HomePage />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
